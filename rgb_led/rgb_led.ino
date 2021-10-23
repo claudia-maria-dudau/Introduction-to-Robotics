d907547e20e8681cf6660b413a217f881ffeebe7
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2021-10-23 15:56:04 +0300

    code styling

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -1,14 +1,14 @@
-// pin ports
+// led pins
 const int ledRedPin = 11;
 const int ledGreenPin = 10;
 const int ledBluePin = 9;
 
-//potentiator ports
+// potentiometer pins
 const int potRedPin = A0;
 const int potGreenPin = A1;
 const int potBluePin = A2;
 
-// potentiator values
+// potentiometer values
 int potRedValue = 0;
 int potGreenValue = 0;
 int potBlueValue = 0;
@@ -28,25 +28,37 @@ void setup() {
   pinMode(ledGreenPin, OUTPUT);
   pinMode(ledBluePin, OUTPUT);
 
-  // potentiator pins are in INPUT mode
+  // potentiometer pins are in INPUT mode
   pinMode(potRedPin, INPUT);
   pinMode(potGreenPin, INPUT);
   pinMode(potBluePin, INPUT);
 }
 
 void loop() {
-  // reading potentiator values
+  readPotValues();
+
+  calculateBrightness();
+
+  setLedValues();
+}
+
+// read value of each potentiometer
+void readPotValues() {
   potRedValue = analogRead(potRedPin);
   potGreenValue = analogRead(potGreenPin);
   potBlueValue = analogRead(potBluePin);
+}
 
-  // calculating brightness
-  // maping the values from [0..1023] (potentiator) to [0..255] (led)
+// calculate brightness of each color
+void calculateBrightness() {
+  // maping the values from [0..1023] (potentiometer) to [0..255] (led)
   brightnessRed = map(potRedValue,  0,  maxPotValue,  0,  maxLedValue);
   brightnessGreen = map(potGreenValue,  0,  maxPotValue,  0,  maxLedValue);
   brightnessBlue = map(potBlueValue,  0,  maxPotValue,  0,  maxLedValue);
+}
 
-  // writing led values
+// set color of the RGB led
+void setLedValues() {
   analogWrite(ledRedPin, brightnessRed);
   analogWrite(ledGreenPin, brightnessGreen);
   analogWrite(ledBluePin, brightnessBlue);
