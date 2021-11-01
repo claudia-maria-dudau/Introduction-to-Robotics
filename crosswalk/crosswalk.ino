77dbb6d4fc47f79b99fc78949dd0c49c2b5a8aab
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2021-11-01 11:43:14 +0200

    crosswalk sound
    Adding sound for the pedestrian semaphore

--- crosswalk/crosswalk.ino
+++ crosswalk/crosswalk.ino
@@ -11,6 +11,9 @@ const int ledRedPin2 = 6;
 // push button 
 const int pushButton = 7;
 
+// passive buzzer
+const int buzzerPin = 11;
+
 // led states (green for cars & red for pedestrians)
 bool ledRedState1 = LOW;
 bool ledYellowState1 = LOW;
@@ -19,6 +22,7 @@ bool ledGreenState1 = HIGH;
 bool ledRedState2 = HIGH;
 bool ledGreenState2 = LOW;
 
+// button variables
 bool buttonState = LOW;
 
 // debounce variables
@@ -33,16 +37,23 @@ unsigned int startChange = 0;
 unsigned int elapsedTime = 0;
 
 // all the intervals are relative to the moment of pressing the button
-const int greenToYellowInterval = 5000;
-const int greenToRedCarsInterval = 10000;
-const int carsToPedestriansInterval = 12000;
-const int greenToIntermittentInterval = 22000;
-const int greenToRedPedestriansInterval = 27000;
-const int pedestriansToCarsInterval = 29000;
+const int greenToYellowInterval = 10000;
+const int greenToRedCarsInterval = 13000;
+const int carsToPedestriansInterval = 14000;
+const int greenToIntermittentInterval = 24000;
+const int greenToRedPedestriansInterval = 29000;
+const int pedestriansToCarsInterval = 30000;
 
 // intermitten lighting variables
-unsigned int lastChanged = 0;
+unsigned int lastChangedLed = 0;
 const int intermittenInterval = 500;
+
+// buzzer variables
+int buzzerTone = 5000;
+int toneDuration = 10;
+const int buzzerDelayGreen = 1000;
+const int buzzerDelayIntermittenGreen = 500;
+unsigned int lastChangedBuzzer = 0;
 
 void setup() {
   // button pin is in INPUT mode
@@ -54,6 +65,9 @@ void setup() {
   pinMode(ledGreenPin1, OUTPUT);
   pinMode(ledRedPin2, OUTPUT);
   pinMode(ledGreenPin2, OUTPUT);
+
+  // buzzer pin is in OUTPUT mode
+  pinMode(buzzerPin, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -80,14 +94,14 @@ void readButton() {
   if (reading != lastReading) {
     lastDebounceTime = millis();
   }
-
-  /* trying to eliminate the possible noise by waiting a few miliseconds
-  in order to make sure the button was pressed intentionally */
+  Serial.println(reading);
+  // trying to eliminate the possible noise by waiting a few miliseconds
+  // in order to make sure the button was pressed intentionally 
   if (millis() - lastDebounceTime > debounceInterval) {
     if (reading != buttonState) {
       buttonState = reading;
       // changing semaphore to give priority to pedestrians
-      if (buttonState == HIGH && !pedestrianSemaphoreOn) {
+      if (buttonState == LOW && !pedestrianSemaphoreOn) {
         pedestrianSemaphoreOn = true;
         startChange = millis();
       }
@@ -101,12 +115,13 @@ void readButton() {
 void pedestrianSemaphore() {
   elapsedTime = millis();
   
-  if (elapsedTime- startChange > pedestriansToCarsInterval) {
+  if (elapsedTime - startChange > pedestriansToCarsInterval) {
     // CARS: green
     // PEDESTRAINS: red
     ledRedState1 = LOW;
     ledGreenState1 = HIGH;
-    
+
+    // end of semaphores flow
     pedestrianSemaphoreOn = false;
   } else if (elapsedTime - startChange > greenToRedPedestriansInterval) {
     // CARS: red
@@ -116,22 +131,34 @@ void pedestrianSemaphore() {
   } else if (elapsedTime - startChange > greenToIntermittentInterval) {
     // CARS: red
     // PEDESTRIANS: intermittent green  
-    if (elapsedTime - lastChanged > intermittenInterval) {
+    if (elapsedTime - lastChangedLed > intermittenInterval) {
       ledGreenState2 = !ledGreenState2;
-      lastChanged = millis();
+      lastChangedLed = millis();
+    }
+
+    // making the buzzer emit a sound at a given time interval
+    if (elapsedTime - lastChangedBuzzer > buzzerDelayIntermittenGreen) {
+      tone(buzzerPin, buzzerTone, toneDuration);
+      lastChangedBuzzer = millis();
     }
   } else if (elapsedTime - startChange > carsToPedestriansInterval) { 
     // CARS: red
     // PEDESTRIANS: green
     ledRedState2 = LOW;
     ledGreenState2 = HIGH;
+
+    // making the buzzer emit a sound at a given time interval
+    if (elapsedTime - lastChangedBuzzer > buzzerDelayGreen) {
+      tone(buzzerPin, buzzerTone, toneDuration);
+      lastChangedBuzzer = millis();
+    }
   } else if (elapsedTime - startChange > greenToRedCarsInterval) {
-    // CARS: yellow to red
+    // CARS: red
     // PEDESTRIANS: red
     ledYellowState1 = LOW;
     ledRedState1 = HIGH;
   } else if (elapsedTime - startChange > greenToYellowInterval) {
-    // CARS: green to yellow 
+    // CARS: yellow 
     // PEDESTRIANS: red
     ledGreenState1 = LOW;
     ledYellowState1 = HIGH;
